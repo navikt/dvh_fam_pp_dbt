663576daefdd3324bb44b7cd5fef90e2b5ad40a4
Adam Arafa Røe <adam.arafa.roe@nav.no>
2025-06-02 09:58:20 +0200

    henter siste diagnose basert på behandlings_id

--- models/PP_utpakking/fam_pp_diagnose.sql
+++ models/PP_utpakking/fam_pp_diagnose.sql
@@ -25,11 +25,10 @@ select * from pp_meta_data,
   ) j
   where kode is not null
 ),
- 
+
 pre_final2 as (
   select p.kode
         ,p.type
-        --,p.vedtaks_tidspunkt
         ,f.pk_pp_fagsak as fk_pp_fagsak
         ,f.forrige_behandlings_id, f.behandlings_id, f.saksnummer
         ,f.vedtaks_tidspunkt
@@ -37,53 +36,69 @@ pre_final2 as (
   join pp_fagsak f
   on p.kafka_offset = f.kafka_offset
 ),
- 
-ny_diagnose as
-(
-  select ny.saksnummer,diagnose_tidligere.type,diagnose_tidligere.kode, count(distinct fagsak_tidligere.behandlings_id) antall_behandlinger
+diagnose_fagsak_alle AS (
+select ny.saksnummer,ny.behandlings_id siste_behandlings_id,fagsak_tidligere.behandlings_id,diagnose_tidligere.type,diagnose_tidligere.kode
   from pre_final2 ny
   left outer join pp_fagsak fagsak_tidligere On
   ny.saksnummer=fagsak_tidligere.saksnummer and
-  ny.vedtaks_tidspunkt>fagsak_tidligere.vedtaks_tidspunkt
- 
-  left outer join fam_pp_diagnose diagnose_tidligere on
+  ny.vedtaks_tidspunkt > fagsak_tidligere.vedtaks_tidspunkt
+  join fam_pp_diagnose diagnose_tidligere on
   diagnose_tidligere.fk_pp_fagsak=fagsak_tidligere.pk_pp_fagsak
   and  diagnose_tidligere.type=ny.type
   and diagnose_tidligere.kode=ny.kode
-  group by  ny.saksnummer,diagnose_tidligere.type,diagnose_tidligere.kode
-),
+  UNION ALL
+  select saksnummer,behandlings_id siste_behandlings_id ,behandlings_id,type,kode from
+  pre_final2
+)
+,ny_diagnose as
+(
+  select saksnummer,siste_behandlings_id ,type,kode, count(distinct behandlings_id) antall_behandlinger
+  from diagnose_fagsak_alle
+  group by  saksnummer,siste_behandlings_id ,type,kode
  
+),
 min_antall as
 (
-select min(antall_behandlinger) min_antall_behandlinger from ny_diagnose
+select siste_behandlings_id ,min(antall_behandlinger) min_antall_behandlinger,max(antall_behandlinger)  max_antall_behandlinger from ny_diagnose
+group by siste_behandlings_id
+ 
 )
 ,
 final as
 (
   select ny.*
-         , nvl(antall_behandlinger,0) antall_behandlinger
+         , antall_behandlinger antall_behandlinger
          ,min_antall.min_antall_behandlinger
-         ,case when nvl(antall_behandlinger,0)<=min_antall_behandlinger then 'J' else 'N' end siste_diagnose_flagg
-        
+         ,case  when nvl(antall_behandlinger,0)=0 then 'J'
+                when  antall_behandlinger <= min_antall.min_antall_behandlinger then 'J'
+         else 'N' end siste_diagnose_flagg
   from pre_final2 ny
   left join ny_diagnose
   on ny.saksnummer = ny_diagnose.saksnummer
   and ny.kode = ny_diagnose.kode
   and ny.type = ny_diagnose.type
- 
   left join min_antall on
-  1=1
- 
-)
- 
+  min_antall.siste_behandlings_id = ny_diagnose.siste_behandlings_id
+),
+
+fk_diagnose as (
 select
-dvh_fampp_kafka.hibernate_sequence.nextval as pk_pp_diagnose,
-kode,
-type
-,fk_pp_fagsak
-,localtimestamp as lastet_dato
-,cast(null as varchar2(4)) as fk_dim_diagnose
---,antall_behandlinger
---,min_antall_behandlinger
-,siste_diagnose_flagg
-from final
+    f.*,
+    pk_dim_diagnose fk_dim_diagnose
+    from final f
+      join dt_p.dim_diagnose
+      on f.kode = dim_diagnose.diagnose_kode
+      and f.type = upper(dim_diagnose.diagnose_tabell)
+      and f.vedtaks_tidspunkt between dim_diagnose.gyldig_fra_dato and dim_diagnose.gyldig_til_dato
+)
+
+select
+  dvh_fampp_kafka.hibernate_sequence.nextval as pk_pp_diagnose
+  ,kode
+  ,type
+  ,fk_pp_fagsak
+  ,localtimestamp as lastet_dato
+  ,fk_dim_diagnose
+  ,siste_diagnose_flagg
+from fk_diagnose
+ 
